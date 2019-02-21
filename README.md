eedbdfbd751340e04f697ee8d2857999bb53f839
lunksana <zoufeng4@gmail.com>
2019-02-21 16:37:15 +0800

    day1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # docker-v2ray
 ## 1.说明
-* V2ray
+* V2ray 轻量化docker镜像
 * Caddy
--- start.sh
+++ start.sh
@@ -2,4 +2,6 @@
 
 #获取服务器IP
 osip=$(curl -4s https://api.ip.sb/ip)
+#自动生成UUID
+uuid=$(cat /proc/sys/kernel/random/uuid)
 
